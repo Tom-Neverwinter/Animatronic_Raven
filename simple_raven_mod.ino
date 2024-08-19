1f91a191ca9b23f3cae03e32848cf403c5bb787f
Tom-Neverwinter <Tom_Neverwinter@yahoo.com>
2024-08-18 19:55:48 -0500

    expand configs from wifi portal
    add some percentiles people can alter
    
    needs testing

--- simple_raven_mod.ino
+++ simple_raven_mod.ino
@@ -22,13 +22,23 @@ CRGB leds[NUM_LEDS];
 int pos = 0;    // Current servo position
 volatile bool active = false;
 
-// Settings
+// Expanded Settings
 struct Settings {
   uint32_t eyeColor;
-  // Add other settings here
+  int blinkProbability;
+  int twitchProbability;
+  int soundProbability;
+  int minSoundDuration;
+  int maxSoundDuration;
 } settings;
 
 WiFiManager wifiManager;
+WiFiManagerParameter custom_eye_color("eye_color", "Eye Color (hex)", "", 7);
+WiFiManagerParameter custom_blink_prob("blink_prob", "Blink Probability (%)", "", 3);
+WiFiManagerParameter custom_twitch_prob("twitch_prob", "Twitch Probability (%)", "", 3);
+WiFiManagerParameter custom_sound_prob("sound_prob", "Sound Probability (%)", "", 3);
+WiFiManagerParameter custom_min_sound_duration("min_sound", "Min Sound Duration (ms)", "", 4);
+WiFiManagerParameter custom_max_sound_duration("max_sound", "Max Sound Duration (ms)", "", 4);
 
 void setup() 
 {
@@ -58,20 +68,44 @@ void setup()
   EEPROM.get(0, settings);
 
   // Setup WiFiManager
-  wifiManager.autoConnect("BirdConfig");
-
-  // Setup custom parameters
-  WiFiManagerParameter custom_eye_color("eye_color", "Eye Color (hex)", String(settings.eyeColor, HEX).c_str(), 7);
   wifiManager.addParameter(&custom_eye_color);
+  wifiManager.addParameter(&custom_blink_prob);
+  wifiManager.addParameter(&custom_twitch_prob);
+  wifiManager.addParameter(&custom_sound_prob);
+  wifiManager.addParameter(&custom_min_sound_duration);
+  wifiManager.addParameter(&custom_max_sound_duration);
 
   // Set config save notify callback
   wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+  // Attempt to connect to WiFi or start config portal
+  if (!wifiManager.autoConnect("BirdConfig")) {
+    Serial.println("Failed to connect and hit timeout");
+    delay(3000);
+    ESP.restart();
+  }
+
+  // If you get here you have connected to the WiFi
+  Serial.println("Connected to WiFi");
 }
 
 void loop() 
 {
-  // Your existing loop code here
-  // ...
+  if (active) {
+    for (int i = 0; i < 10; i++) {
+      int target = random(MIN_POS, MAX_POS);
+      
+      birdMove(target);
+      maybeBlink();
+      maybeTwitch();
+      makeSound();
+      
+      delay(500);
+    }
+    
+    turnOffEyes();
+    active = false;
+  }
 
   // Handle WiFiManager
   wifiManager.process();
@@ -82,15 +116,23 @@ void triggerIsr() {
 }
 
 void birdMove(int dest) {
-  // Your existing birdMove code here
+  // Existing birdMove code...
 }
 
 void maybeTwitch() {
-  // Your existing maybeTwitch code here
+  if (random(100) < settings.twitchProbability) {
+    int timesTwitch = random(3, 6);
+    for (int i = 0; i < timesTwitch; i++) {
+      int currentPos = myservo.read();
+      int twitchAmount = random(-10, 11);
+      int target = constrain(currentPos + twitchAmount, MIN_POS, MAX_POS);
+      birdMove(target);
+    }
+  }
 }
 
 void maybeBlink() {
-  if (random(2) == 0) {
+  if (random(100) < settings.blinkProbability) {
     blink();
     if (random(5) == 0) {
       delay(200);
@@ -105,18 +147,35 @@ void blink() {
   leds[1] = eyeColor;
   FastLED.show();
   delay(100);
+  turnOffEyes();
+}
+
+void turnOffEyes() {
   leds[0] = CRGB::Black;
   leds[1] = CRGB::Black;
   FastLED.show();
 }
 
 void makeSound() {
-  // Your existing makeSound code here
+  if (random(100) < settings.soundProbability) {
+    digitalWrite(SOUND_PIN, HIGH);
+    delay(random(settings.minSoundDuration, settings.maxSoundDuration + 1));
+    digitalWrite(SOUND_PIN, LOW);
+  }
 }
 
 void saveConfigCallback() {
-  Serial.println("Should save config");
-  strcpy(settings.eyeColor, custom_eye_color.getValue());
+  Serial.println("Saving configuration");
+  
+  settings.eyeColor = strtoul(custom_eye_color.getValue(), NULL, 16);
+  settings.blinkProbability = atoi(custom_blink_prob.getValue());
+  settings.twitchProbability = atoi(custom_twitch_prob.getValue());
+  settings.soundProbability = atoi(custom_sound_prob.getValue());
+  settings.minSoundDuration = atoi(custom_min_sound_duration.getValue());
+  settings.maxSoundDuration = atoi(custom_max_sound_duration.getValue());
+
   EEPROM.put(0, settings);
   EEPROM.commit();
+
+  Serial.println("Configuration saved");
 }
